5835520bd6d4f396b1a52b65c12ef8c2ba0977b3
Cameron <cameron@biteable.com>
2017-10-06 17:08:31 +1100

    added some seed data

---
+++ src/store/types.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import * as actions from './actions'
+import * as getters from './getters'
+import mutations from './mutations'
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+export default new Vuex.Store({
+  actions,
+  getters,
+  mutations,
+  state: {
+    board: null,
+    clues: null
+  },
+  strict: debug
+})
