02bd044d357524d08746b63a1c1cfbfdec21b4dd
Cameron <cameron@biteable.com>
2017-10-09 10:38:00 +1100

    fixed hue getter

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,10 +4,10 @@ export const game = ({ game }) => game
 export const boardId = ({ boardId }) => boardId
 export const uid = ({ uid }) => uid
 export const stringToHSL = () => (str) => {
-  const code = str.split('').reduce((hash, char) => {
-    hash = char + ((hash << 5) - hash)
+  const num = str.split('').reduce((hash, char) => {
+    hash = String(char).charCodeAt(0) + ((hash << 5) - hash)
     return hash & hash
   }, 0)
-  const hue = code % 360
+  const hue = Math.abs(num) % 360
   return `hsl(${hue}, 90%, 60%)`
 }
