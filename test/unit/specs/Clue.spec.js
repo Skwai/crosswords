90ecd5ae7dedf0a242b4f582ef6ef03531fb93eb
Cameron <cameron@biteable.com>
2017-10-19 21:50:15 +1100

    mocked store

--- test/unit/specs/Clue.spec.js
+++ test/unit/specs/Clue.spec.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import Clue from '@/components/Clue'
 
+const store = {
+  getters: {
+    uid: () => 'user1',
+    isFocusedWord: () => false,
+    stringToHSL: (opacity) => `hsla(100, 90%, 60%, ${opacity})`
+  }
+}
+
 describe('Clue.vue', () => {
   it('should render a clue', () => {
-    const vm = new Vue(Clue).$mount()
+    const vm = new Vue({ store, ...Clue }).$mount()
     expect(vm.$el instanceof Node).is.true
   })
 })
